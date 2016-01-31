aa1b0bc67768f1147cf19a3d34b05fdc369a8fe4
Julien Charbonneau <julien_charbonneau@live.ca>
2016-01-31 15:34:17 -0500

    Base map structure algorithm completed, visual color sprite for walls and doors

--- Pixel Hero/Assets/Scripts/Map/BaseMap.cs
+++ Pixel Hero/Assets/Scripts/Map/BaseMap.cs
@@ -6,6 +6,8 @@ public class BaseMap : MonoBehaviour
 {
     public List<GameObject> floorList = new List<GameObject>();
     public List<GameObject> borderList = new List<GameObject>();
+    public GameObject door;
+    public GameObject wall;
     public GameObject SpawnPoint;
     public GameObject player;
 
@@ -15,6 +17,7 @@ public class BaseMap : MonoBehaviour
     public int roomGridY = 10;
     public int numberOfRoom = 15;
     public int groupingFactor = 100;
+    public int maxNeighbours = 3;
 
     // Use this for initialization
     void Start()
@@ -39,33 +42,52 @@ public class BaseMap : MonoBehaviour
             int i = Random.Range(1, roomGridY - 1);
             int j = Random.Range(1, roomGridX - 1);
 
-            if (tabRooms[i, j].GetType() != typeof(NotARoom) && !tabRooms[i, j].IsScanned)
+            if (tabRooms[i, j].GetType() != typeof(NotARoom))
             {
                 if ((tabRooms[i + 1, j].GetType() == typeof(NotARoom)) && (Random.Range(0f, 100) <= groupingFactor) && (roomCounter < numberOfRoom))
                 {
-                    tabRooms[i + 1, j] = new StandartRoom(roomWidth, roomHeight, j * roomWidth, (i+1) * roomHeight);
-                    tabRooms[i, j].ConnectRoom(ref tabRooms[i + 1, j]);
-                    roomCounter++;
+                    tabRooms[i + 1, j] = new StandartRoom(roomWidth, roomHeight, j * roomWidth, (i + 1) * roomHeight);
+                    if (TooManyNeighbours(tabRooms))
+                        tabRooms[i + 1, j] = new NotARoom(roomWidth, roomHeight, j * roomWidth, (i + 1) * roomHeight);
+                    else
+                    {
+                        tabRooms[i, j].ConnectRoom(ref tabRooms[i + 1, j]);
+                        roomCounter++;
+                    }
                 }
                 if ((tabRooms[i - 1, j].GetType() == typeof(NotARoom)) && (Random.Range(0f, 100) <= groupingFactor) && (roomCounter < numberOfRoom))
                 {
-                    tabRooms[i - 1, j] = new StandartRoom(roomWidth, roomHeight, j * roomWidth, (i-1) * roomHeight);
-                    tabRooms[i, j].ConnectRoom(ref tabRooms[i - 1, j]);
-                    roomCounter++;
+                    tabRooms[i - 1, j] = new StandartRoom(roomWidth, roomHeight, j * roomWidth, (i - 1) * roomHeight);
+                    if (TooManyNeighbours(tabRooms))
+                        tabRooms[i - 1, j] = new NotARoom(roomWidth, roomHeight, j * roomWidth, (i - 1) * roomHeight);
+                    else
+                    {
+                        tabRooms[i, j].ConnectRoom(ref tabRooms[i - 1, j]);
+                        roomCounter++;
+                    }
                 }
                 if ((tabRooms[i, j + 1].GetType() == typeof(NotARoom)) && (Random.Range(0f, 100) <= groupingFactor) && (roomCounter < numberOfRoom))
                 {
-                    tabRooms[i, j + 1] = new StandartRoom(roomWidth, roomHeight, (j+1) * roomWidth, i * roomHeight);
-                    tabRooms[i, j].ConnectRoom(ref tabRooms[i, j + 1]);
-                    roomCounter++;
+                    tabRooms[i, j + 1] = new StandartRoom(roomWidth, roomHeight, (j + 1) * roomWidth, i * roomHeight);
+                    if (TooManyNeighbours(tabRooms))
+                        tabRooms[i, j + 1] = new NotARoom(roomWidth, roomHeight, (j + 1) * roomWidth, i * roomHeight);
+                    else
+                    {
+                        tabRooms[i, j].ConnectRoom(ref tabRooms[i, j + 1]);
+                        roomCounter++;
+                    }
                 }
                 if ((tabRooms[i, j - 1].GetType() == typeof(NotARoom)) && (Random.Range(0f, 100) <= groupingFactor) && (roomCounter < numberOfRoom))
                 {
-                    tabRooms[i, j - 1] = new StandartRoom(roomWidth, roomHeight, (j-1) * roomWidth, i * roomHeight);
-                    tabRooms[i, j].ConnectRoom(ref tabRooms[i, j - 1]);
-                    roomCounter++;
+                    tabRooms[i, j - 1] = new StandartRoom(roomWidth, roomHeight, (j - 1) * roomWidth, i * roomHeight);
+                    if (TooManyNeighbours(tabRooms))
+                        tabRooms[i, j - 1] = new NotARoom(roomWidth, roomHeight, (j - 1) * roomWidth, i * roomHeight);
+                    else
+                    {
+                        tabRooms[i, j].ConnectRoom(ref tabRooms[i, j - 1]);
+                        roomCounter++;
+                    }
                 }
-                tabRooms[i, j].IsScanned = true;
             }
         }
     }
@@ -94,7 +116,7 @@ public class BaseMap : MonoBehaviour
         {
             for (int j = 0; j < roomGridX; j++)
             {
-                InstanciateTiles(tabRooms[i,j]);
+                InstanciateTiles(tabRooms[i, j]);
             }
         }
     }
@@ -105,17 +127,23 @@ public class BaseMap : MonoBehaviour
         {
             for (int j = 0; j < roomWidth; j++)
             {
-                switch (room.getTile(i,j))
+                switch (room.getTile(i, j))
                 {
                     case 'D':
+                        GameObject doorClone = (GameObject)Instantiate(door, new Vector3(room.GridPosX + j, room.GridPosY + i, 0), Quaternion.identity);
+                        doorClone.transform.parent = transform;
+                        break;
                     case 'F':
                         GameObject floorClone = (GameObject)Instantiate(floorList[Random.Range(0, floorList.Count - 1)], new Vector3(room.GridPosX + j, room.GridPosY + i, 0), Quaternion.identity);
                         floorClone.transform.parent = transform;
                         break;
                     case 'N':
+                        GameObject nullClone = (GameObject)Instantiate(borderList[Random.Range(0, borderList.Count - 1)], new Vector3(room.GridPosX + j, room.GridPosY + i, 0), Quaternion.identity);
+                        nullClone.transform.parent = transform;
+                        break;
                     case 'W':
-                        GameObject boundClone = (GameObject)Instantiate(borderList[Random.Range(0, borderList.Count - 1)], new Vector3(room.GridPosX + j, room.GridPosY + i, 0), Quaternion.identity);
-                        boundClone.transform.parent = transform;
+                        GameObject wallClone = (GameObject)Instantiate(wall, new Vector3(room.GridPosX + j, room.GridPosY + i, 0), Quaternion.identity);
+                        wallClone.transform.parent = transform;
                         break;
                     case 'H':
                         SpawnPoint.transform.position = new Vector3(room.GridPosX + j, room.GridPosY + i, 0);
@@ -125,5 +153,30 @@ public class BaseMap : MonoBehaviour
                 }
             }
         }
+    }
+
+    bool TooManyNeighbours(Room[,] tabRooms)
+    {
+        for (int i = 1; i < roomGridY - 1; i++)
+        {
+            for (int j = 1; j < roomGridX - 1; j++)
+            {
+                int roomCount = 0;
+
+                if (tabRooms[i + 1, j].GetType() != typeof(NotARoom))
+                    roomCount++;
+                if (tabRooms[i - 1, j].GetType() != typeof(NotARoom))
+                    roomCount++;
+                if (tabRooms[i, j + 1].GetType() != typeof(NotARoom))
+                    roomCount++;
+                if (tabRooms[i, j - 1].GetType() != typeof(NotARoom))
+                    roomCount++;
+
+                if (roomCount > maxNeighbours)
+                    return true;
+
+            }
+        }
+        return false;
     }
 }
